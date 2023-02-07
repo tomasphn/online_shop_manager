07e1b0f45b70ed1f4a4fc8f08c24e9d2fb35571c
tomasphn <combomeal12@gmail.com>
2023-02-06 17:16:51 -0800

    remove env route for security

--- app/shops.rb
+++ app/shops.rb
@@ -28,10 +28,6 @@ before do
   @database = ShopDatabase.new(logger)
 end
 
-get '/env' do
-  ENV.map { |k,v| "#{k}: #{v}" }.join("<br>")
-end
-
 # Sign in page
 get "/signin" do
   redirect "/" if user_signed_in?
